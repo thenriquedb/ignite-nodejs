5bfa948391a3cfe0c609f664cc25ffb4ee2185be
thiago <thenrique2012@gmail.com>
2022-07-19 20:07:29 -0300

    feat: create account

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 const express = require("express");
+const { v4: uuidV4 } = require("uuid");
+
+const app = express();
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf: string;
+ * name: string;
+ * id: uuid;
+ * statement: [];
+ */
+app.post("/accounts", (req, res) => {
+  const { cpf, name } = req.body;
+  const id = uuidV4();
+
+  const customer = {
+    cpf,
+    name,
+    id,
+    statement: [],
+  };
+
+  customers.push(customer);
+
+  return res.status(201).send();
+});
+
+app.listen(3333);
