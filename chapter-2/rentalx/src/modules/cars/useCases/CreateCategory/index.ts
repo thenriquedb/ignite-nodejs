d73b50a2f7c0fdaa68d911e4003a24a372005333
thiago <thenrique2012@gmail.com>
2022-07-23 15:17:34 -0300

    refactor(chapter-2/rentalx): implement factory pattern to create category use case

--- chapter-2/rentalx/src/modules/cars/useCases/CreateCategory/index.ts
+++ chapter-2/rentalx/src/modules/cars/useCases/CreateCategory/index.ts
@@ -1,12 +1,1 @@
-import { CategoriesRepository } from "@modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreateCategoryController";
-
-import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
-
-const categoriesRepository = new CategoriesRepository();
-const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-const createCategoryController = new CreateCategoryController(
-  createCategoryUseCase
-);
-
-export { createCategoryController };
+export * from "./CreateCategoryFactory";
