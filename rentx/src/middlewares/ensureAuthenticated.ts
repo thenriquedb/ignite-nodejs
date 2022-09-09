9bfdae1f6887d0b0b4a7cec6eef66c84b511728e
thiago <thenrique2012@gmail.com>
2022-09-08 22:50:01 -0300

    feat(rentx): create ensureAuthenticated middleware

---
+++ rentx/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,39 @@
+import { UsersRepository } from "@modules/accounts/repositories/implementations/UsersRepository";
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  _: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error("Token missing");
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "6948894f2dbd850aa19bd64b6247812b"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists!");
+    }
+
+    next();
+  } catch (error) {
+    throw new Error("Invalid token");
+  }
+}
--- rentx/src/routes/index.ts
+++ rentx/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationRoutes } from "./specification.routes";
@@ -8,6 +9,8 @@ import { usersRoutes } from "./users.routes";
 const router = Router();
 
 router.use(authenticateRoutes);
+
+router.use(ensureAuthenticated);
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationRoutes);
 router.use("/users", usersRoutes);
