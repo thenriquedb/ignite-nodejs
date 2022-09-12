5edbeb2470dd2b78e9487d58c35edf1103105830
thiago <thenrique2012@gmail.com>
2022-09-12 09:47:54 -0300

    feat(rentx): create middleware for handle expections

--- rentx/src/server.ts
+++ rentx/src/server.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata";
 import express from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import "./database";
 import "./shared/container";
+import { errorHandler } from "./middlewares/errorHandler";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -12,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
 app.use(router);
+app.use(errorHandler);
 
 app.listen(3333);
