79ce8a0b9a7fcc9a930a63d7138c976e57408e86
thiago <thenrique2012@gmail.com>
2022-09-12 20:38:35 -0300

    test(rentx): implement UsersRepositoryInMemory for tests

---
+++ rentx/src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,34 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  private users: User[] = [];
+
+  async create(data: ICreateUserDTO): Promise<void> {
+    const { driver_license, email, name, password } = data;
+
+    const user = new User();
+
+    Object.assign(user, {
+      driver_license,
+      email,
+      name,
+      password,
+    });
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find((user) => user.email === email);
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+}
+
+export { UsersRepositoryInMemory };
