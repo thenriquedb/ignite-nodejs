0f97f0bf88577065f16f5d510c8bb44ab7e0c8ab
thiago <thenrique2012@gmail.com>
2022-09-12 17:35:00 -0300

    test(rentx): implement CategoriesRepositoryInMemory for tests

---
+++ rentx/src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,34 @@
+import { Category } from "@modules/cars/entities/Category";
+
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  private categories: Category[] = [];
+
+  async create(data: ICreateCategoryDTO) {
+    const { name, description } = data;
+
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+
+  async findByName(name: string) {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
+
+  async list() {
+    return this.categories;
+  }
+}
+
+export { CategoriesRepositoryInMemory };
